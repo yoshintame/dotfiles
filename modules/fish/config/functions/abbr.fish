8c2c968f677ce1b9a927d91292bd675972cc1b29
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-05-16 14:42:55 +0300

    feat(fish): update abbr

--- modules/fish/config/functions/abbr.fish
+++ modules/fish/config/functions/abbr.fish
@@ -1,5 +1,10 @@
 alias cf 'zi'
 alias c 'z'
+alias f="yy"
+alias fc "fzf"
+abbr grep="rg"
+abbr cat="bat"
+
 alias .. 'z ..'
 alias ... 'z ../..'
 alias .... 'z ../../..'
@@ -12,89 +17,80 @@ alias - 'z -'
 alias c.. "z .."
 alias cd.. "z .."
 
-abbr md 'mkdir -p'
-abbr rd 'rmdir'
 abbr mkdir 'mkdir -p'
+abbr md 'mkdir -p'
 
+abbr rd 'rmdir'
 abbr rm 'rm -r'
+abbr rmf 'rm -rf'
 abbr rmrf 'rm -rf'
 
-abbr cp 'cp -r'
+abbr del "trash"
+abbr sdel "sudo rm -rf"
 
+abbr cp 'cp -r'
 abbr ln 'ln -s'
 
-alias grep="grep --color"
-
-abbr edit "cursor"
-abbr e "cursor"
+alias edit "cursor"
+alias e "cursor"
 alias ee="cursor ."
 abbr o "open"
 alias oo="open ."
-abbr del "trash"
-abbr sdel "sudo rm -rf"
+
 alias cpwd='pwd | tr -d "\n" | pbcopy'
 
-# lf
-abbr ld "lfcd"
-
-# cat (bat)
-alias cat="bat"
-
 # brew
-abbr binst "brew install"
-abbr bi "brew install"
+alias binst "brew install"
+alias bi "brew install"
 
 # node
 abbr n "npm"
-abbr ninst "npm install"
-abbr ni "npm install"
+alias ninst "npm install"
+alias ni "npm install"
 alias nd="npm run dev"
 alias ns="npm run start"
 abbr nr="npm run"
 
 abbr pn "pnpm"
-abbr pninst "pnpm install"
-abbr pni "pnpm install"
+alias pninst "pnpm install"
+alias pni "pnpm install"
 alias pnd="pnpm dev"
 alias pns="pnpm start"
-alias px "pnpx"
+abbr px "pnpx"
 
 abbr b "bun"
-abbr binst "bun install"
-abbr bi "bun install"
+alias binst "bun install"
+alias bi "bun install"
 alias bd="bun run dev"
 alias bs="bun run start"
 abbr bx="bun exec"
 abbr br="bun run"
 
-
 # python
 abbr py "python"
 abbr python "python3"
 abbr pip "pip3"
-abbr pinst "pip3 install"
-abbr pi "pip3 install"
+alias pinst "pip3 install"
+alias pi "pip3 install"
 
 # network
-abbr ip "dig +short myip.opendns.com @resolver1.opendns.com"
-abbr localip "ipconfig getifaddr en0"
-abbr ips "ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
-abbr sniff "sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-abbr httpdump "sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-abbr lssh "grep -w -i Host ~/.ssh/config | sed s/Host//"
-abbr -a wget "wget -c"
+alias ip "dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip "ipconfig getifaddr en0"
+alias ips "ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
+alias sniff "sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump "sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+alias lssh "grep -w -i Host ~/.ssh/config | sed s/Host//"
+alias -a wget "wget -c"
 
 # fast edit
-abbr edithostfile "sudo $EDITOR /etc/hosts"
-abbr editssh "sudo $EDITOR ~/.ssh"
+alias edithostfile "sudo $EDITOR /etc/hosts"
+alias editssh "sudo $EDITOR ~/.ssh"
 
 # fast cd
-abbr dev "cd ~/Development"
-abbr dotfiles "cd ~/.dotfiles"
+alias dev "cd ~/Development"
+alias dotfiles "cd ~/.dotfiles"
 
 # dotfiles
 alias dotapply="dotbot -c ~/.dotfiles/install.conf.yaml"
 alias dotedit="cursor ~/.dotfiles"
 
-# yazi
-alias f="yy"

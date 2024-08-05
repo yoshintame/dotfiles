bd22cb8a105de8ab691938fcfcf3d3d46f0595f8
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-05 21:04:00 +0300

    feat(yazi): actualize to yazi version 3.0

--- modules/yazi/config/init.lua
+++ modules/yazi/config/init.lua
@@ -1,16 +1,3 @@
-function Manager:render(area)
-	local chunks = self:layout(area)
-
-	return ya.flat {
-		-- Parent
-        Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
-		-- Current
-        Current:render(chunks[2]:padding(ui.Padding.y(1))),
-		-- Preview
-        Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
-	}
-end
-
 require("zoxide"):setup { update_db = true }
 require("starship"):setup()
 require("relative-motions"):setup({ show_numbers="relative", show_motion = true })

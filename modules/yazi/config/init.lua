ea521961754caabf79027b1baff318b2ce33c88d
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-06 22:43:29 +0300

    feat(yazi): add full-bodred plugin and return ouch previewer

--- modules/yazi/config/init.lua
+++ modules/yazi/config/init.lua
@@ -1,3 +1,4 @@
 require("zoxide"):setup { update_db = true }
 require("starship"):setup()
 require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
+require("full-border"):setup { type = ui.Border.ROUNDED }

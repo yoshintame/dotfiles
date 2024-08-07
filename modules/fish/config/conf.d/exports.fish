eb6d00f88004fbaabe554e723fe172cf44eebcde
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-07 13:41:37 +0300

    feat(fish): add utils func and grc integration

---
+++ modules/fish/config/conf.d/exports.fish
@@ -0,0 +1,9 @@
+# set default key bindings
+set -g fish_key_bindings fish_default_key_bindings
+
+# Source GRC
+source /opt/homebrew/etc/grc.fish
+
+# Source Multi-function files
+source ~/.config/fish/functions/_utils.fish
+source ~/.config/fish/functions/_backup_restore.fish

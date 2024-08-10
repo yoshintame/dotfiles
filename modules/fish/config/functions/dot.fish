c34fbf332f97f6538eb1db6337a9a4f00e070d6c
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-11 02:16:13 +0300

    feat(fish): add to dot util help screen

--- modules/fish/config/functions/dot.fish
+++ modules/fish/config/functions/dot.fish
@@ -1,12 +1,21 @@
-function dot
-    switch $argv[1]
+function dot --argument-names cmd --description "Dotfiles managment wrapper"
+    switch "$cmd"
+        case "" -h --help
+            echo "Usage: dot <command>"
+            echo ""
+            echo "Commands:"
+            echo "  link   - Run dotbot with the specified configuration"
+            echo "  go     - Change directory to the dotfiles directory"
+            echo "  edit   - Open the dotfiles directory in the default editor"
+            echo "  help   - Display this help message"
+            return 0
         case 'link'
             dotbot -c $DOTFILES_CONFIG -d $DOTFILES
         case 'go'
             cd $DOTFILES
         case 'edit'
             $EDITOR $DOTFILES
-        case '*'
-            echo "Usage: dot <link|go|edit>"
+        case \*
+            echo "dot: Unknown command: \"$cmd\"" >&2 && return 1
     end
 end

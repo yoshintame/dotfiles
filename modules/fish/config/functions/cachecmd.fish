f3244f5907d405423188287eaa0d4d0756db2791
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-12 01:36:49 +0300

    feat(fish): add cachecmd

---
+++ modules/fish/config/functions/cachecmd.fish
@@ -0,0 +1,12 @@
+function cachecmd --description "Cache a command"
+    set --local cmdname (
+        string join '-' $argv |
+        string replace -a '/' '_' |
+        string replace -r '^_' ''
+    )
+    set --local cachefile $__fish_cache_dir/$cmdname.fish
+    if not test -f $cachefile
+        $argv >$cachefile
+    end
+    cat $cachefile
+end

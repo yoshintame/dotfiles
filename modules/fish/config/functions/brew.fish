f78423c32b03c920543c0b6f162174fda5135002
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-12 03:05:27 +0300

    feat(fish): add brew `list -o` and `list --D`

--- modules/fish/config/functions/brew.fish
+++ modules/fish/config/functions/brew.fish
@@ -29,10 +29,36 @@ function brew
                 brew bundle dump --file=~/.config/packages/Brewfile --force
             case 'dump'
                 brew bundle dump --file=~/.config/packages/Brewfile --force
+            case 'list'
+                switch $flags
+                    case '-o'
+                        __brew_pretty_list
+                    case '--descriptions' '-D'
+                        __brew_descriptions
+                    case '*'
+                        command brew list $args[2..-1]
+                end
             case '*'
                 command brew $argv
         end
     else
         command brew
     end
+end
+
+function __brew_pretty_list --description "Show brewed formulae"
+    set -l formulae (brew leaves | xargs brew deps --installed --for-each)
+    set -l casks (brew list --cask 2>/dev/null)
+
+    echo (set_color blue)"==>"(set_color --bold normal)" Formulae"(set_color normal)
+    string replace -r '^(.*):(.*)$' '$1'(set_color blue)'$2'(set_color normal) $formulae
+    echo
+    echo (set_color blue)"==>"(set_color --bold normal)" Casks"(set_color normal)
+    printf '%s\n' $casks
+end
+
+function __brew_descriptions -d 'Show descriptions of brew installs'
+    brew leaves |
+        xargs brew desc --eval-all |
+        string replace -r '^(.*:)(\s+[^\[].*)$' '$1'(set_color blue)'$2'(set_color normal)
 end

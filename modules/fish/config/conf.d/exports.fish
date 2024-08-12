45bb4bc73269e498c5f9ee6ecf048c4f9d9c7ddb
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-12 03:05:54 +0300

    refactor(fish): move keybinds to separate file

--- modules/fish/config/conf.d/exports.fish
+++ modules/fish/config/conf.d/exports.fish
@@ -1,6 +1,3 @@
-# set default key bindings
-set -g fish_key_bindings fish_default_key_bindings
-
 # Source GRC
 source /opt/homebrew/etc/grc.fish
 
---
+++ modules/fish/config/conf.d/keybinds.fish
@@ -0,0 +1,1 @@
+set -g fish_key_bindings fish_default_key_bindings

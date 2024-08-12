60a9ae74816b7c5f8ac669d80e92ed8c49f10f04
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-08-12 04:00:44 +0300

    fix(fish): change yazi alias

--- modules/fish/config/functions/f.fish
+++ modules/fish/config/functions/f.fish
@@ -1,4 +1,4 @@
-function j --description "Run yazi and cd to the directory it returns"
+function f --description "Run yazi and cd to the directory it returns"
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]

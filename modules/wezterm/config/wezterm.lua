751178759fe77ffb6b4550ee343d24bd8e3173be
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-09-25 07:56:19 +0300

    feat(wezterm): add catppuccin color scheme

--- modules/wezterm/config/wezterm.lua
+++ modules/wezterm/config/wezterm.lua
@@ -11,12 +11,10 @@ config = {
     window_background_opacity = 0.7,
     macos_window_background_blur = 100,
     cursor_blink_rate = 600,
+    color_scheme = "Catppuccin Frappe",
     colors = {
-        cursor_bg = '#aeaeae',
         foreground = '#ffffff',
         background = '#010101',
-        ansi = { "#121212", "#9834f6", "#682ef5", "#3a2bf5", "#344ef5", "#4481f7", "#5bb6f9", "#f1f1f1" },
-        brights = { "#666666", "#ba5aff", "#905aff", "#695ef6", "#5c78ff", "#5ea2ff", "#5ac8ff", "#ffffff" }
     },
 }
 

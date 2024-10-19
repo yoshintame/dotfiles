7d0ca6d231e3fb036dbff70ce89eb2a7cb545690
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-10-19 17:22:55 +0700

    feat(fish): update and some new abbrs

--- modules/fish/config/conf.d/abbr.fish
+++ modules/fish/config/conf.d/abbr.fish
@@ -5,54 +5,60 @@ end
 # brew
 abbr br "brew"
 abbr bri "brew install"
-abbr brr "brew uninstall"
+abbr bric "brew install --cask"
+abbr brd "brew uninstall"
 abbr brl "brew list"
-abbr nplg 'npm list -g --depth=0'
 abbr bru "brew upgrade"
 abbr brud "brew update"
 abbr brs "brew search"
+abbr brsc "brew search --cask"
 abbr brd "brew dump"
 abbr brup "brew update && brew upgrade && brew cleanup"
 
 # node
 abbr np "npm"
 abbr npi "npm install"
-abbr npr "npm uninstall"
+abbr npd "npm uninstall"
 abbr npu "npm update"
-abbr npd "npm run dev"
-abbr nps "npm run start"
-abbr npb "npm run build"
-abbr npt "npm test"
+abbr npl "npm list"
+abbr nplg 'npm list -g --depth=0'
 abbr npr "npm run"
+abbr nprd "npm run dev"
+abbr nprs "npm run start"
+abbr nprb "npm run build"
+abbr nprt "npm run test"
 
 abbr pn "pnpm"
 abbr pni "pnpm install"
-abbr pnr "pnpm remove"
+abbr pnd "pnpm uninstall"
 abbr pnu "pnpm update"
 abbr pnl "pnpm list"
 abbr pnlg 'pnpm list -g'
-abbr pnd "pnpm dev"
-abbr pns "pnpm start"
-abbr pnb "pnpm build"
-abbr pnt "pnpm test"
+abbr pnr "pnpm run"
+abbr pnrd "pnpm run dev"
+abbr pnrs "pnpm run start"
+abbr pnrb "pnpm run build"
+abbr pnrt "pnpm run test"
+abbr pnrl "pnpm run lint"
+abbr pnrt "pnpm run typecheck"
 abbr pnx "pnpx"
 
 abbr bu "bun"
 abbr bui "bun install"
-abbr bur "bun remove"
+abbr bud "bun remove"
 abbr buu "bun update"
 abbr bulg 'bun pm ls'
 abbr bulg 'bun pm ls -g'
-abbr bud "bun run dev"
-abbr bus "bun run start"
-abbr bub "bun run build"
-abbr but "bun test"
+abbr burd "bun run dev"
+abbr burs "bun run start"
+abbr burb "bun run build"
+abbr burt "bun test"
 
 # fisher
 abbr fi "fisher"
 abbr fii "fisher install"
 abbr fiu "fisher update"
-abbr fir "fisher remove"
+abbr fid "fisher remove"
 abbr fil "fisher list"
 
 # dot

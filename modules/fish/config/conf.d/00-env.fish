6eaba8fa3883a1b7415864fabd92a588b2de87a3
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-10-20 15:10:01 +0700

    feat(fish): change fzf styling

--- modules/fish/config/conf.d/00-env.fish
+++ modules/fish/config/conf.d/00-env.fish
@@ -15,7 +15,13 @@ set -u zoxide_cmd j
 
 # FZF
 set -gx FZF_COMPLETION_OPTS '--color=16 --layout=reverse --inline-info'
-set -gx FZF_DEFAULT_OPTS '--color=16 --layout=reverse --inline-info'
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi
+ --layout=reverse"
 
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH

ead531cc28fdf9207fdfadcb21843238d4440011
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-10-20 15:06:39 +0700

    feat(fish): fix promt init

--- modules/fish/config/conf.d/promt.fish
+++ modules/fish/config/conf.d/promt.fish
@@ -2,13 +2,10 @@ if not status --is-interactive
     exit
 end
 
-# Disable new user greeting.
 set fish_greeting
 
-# Check if starship is installed
 if type -q starship
-    # Initialize starship
-    cachecmd starship init fish | source
+    starship init fish | source
 else
     echo "Warning: starship is not installed"
 end

b28c752bd65c9ac27915b335af7fc8f7510f8f83
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-10-24 11:46:56 +0700

    feat(yazi): add git plugin

--- modules/yazi/config/init.lua
+++ modules/yazi/config/init.lua
@@ -2,3 +2,4 @@ require("zoxide"):setup { update_db = true }
 require("starship"):setup()
 require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
 require("dropover"):setup({ watch_dir="/Users/yoshintame/.local/share/dropover" })
+require("git"):setup()

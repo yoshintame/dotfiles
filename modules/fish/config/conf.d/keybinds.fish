e6e34fbf3c0cdc9cc3d7dca7383be078201a8d5e
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-12-18 15:11:38 +0700

    fix(fish): fix keybinds

--- modules/fish/config/conf.d/keybinds.fish
+++ modules/fish/config/conf.d/keybinds.fish
@@ -1,7 +1,3 @@
-set -g fish_key_bindings fish_default_key_bindings
-
-# Alt + arrow
-bind \e\[1\;3A _fzf_search_history # up
-# bind \e\[1\;3B # down
-# bind \e\[1\;3D # left
-bind \e\[1\;3C ji # right
+bind \e\[3\;3~ kill-word
+bind \e\[1\;3L forward-word
+bind \ez undo

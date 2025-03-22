ceedb23d4d20a6183937337a450be3bb7d0c719a
Mikhail Ivanov <m.ivanov0427@gmail.com>
2025-03-23 01:21:39 +0700

    fix(fish): prevent tmux session creation in vscode and warp

--- modules/fish/config/conf.d/tmux.fish
+++ modules/fish/config/conf.d/tmux.fish
@@ -1,4 +1,7 @@
 if not set -q TMUX
+    and not string match -q "$TERM_PROGRAM" "vscode"
+    and not string match -q "$TERM_PROGRAM" "WarpTerminal"
+
     set -g TMUX tmux new-session -d -s base
     eval $TMUX
     tmux attach-session -d -t base

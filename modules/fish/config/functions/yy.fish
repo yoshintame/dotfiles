8a3ec1bc9d42d09eaa1ccc4107c539b96da73fc7
Mikhail Ivanov <m.ivanov0427@gmail.com>
2026-01-10 01:10:43 +0700

    feat(yazi): add macOS input source switch to ABC on open

--- modules/fish/config/functions/yy.fish
+++ modules/fish/config/functions/yy.fish
@@ -1,4 +1,9 @@
 function yy --description "Run yazi and cd to the directory it returns"
+	# macOS: switch input source to ABC via Hammerspoon
+	if test (uname) = Darwin; and command -q hs
+		command hs -c 'hs.keycodes.currentSourceID("com.apple.keylayout.ABC")' >/dev/null 2>&1
+	end
+
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
 	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]

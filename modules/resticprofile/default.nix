4c295c74496cbb2620f745db1f9eeab139a88e55
Mikhail Ivanov <m.ivanov0427@gmail.com>
2026-03-02 11:14:49 +0700

    feat(resticprofile): enhance backup configuration and logging
    - Added logrotate configuration to manage log file rotation for backup logs.
    - Updated Justfile commands to reflect new naming conventions and improved clarity.
    - Modified profiles.yaml to streamline profile names and enhance scheduling for local and B2 operations.
    - Introduced new links in default.nix for logrotate configuration.

--- modules/resticprofile/default.nix
+++ modules/resticprofile/default.nix
@@ -7,5 +7,6 @@
   nixDotbot.links = {
     "~/.config/resticprofile/profiles.yaml" = "modules/resticprofile/config/profiles.yaml";
     "~/.config/resticprofile/justfile" = "modules/resticprofile/config/justfile";
+    "~/.config/resticprofile/logrotate.conf" = "modules/resticprofile/config/logrotate.conf";
   };
 }

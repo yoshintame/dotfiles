e7361e94b260b97696315906adc294c31faab941
Mikhail Ivanov <m.ivanov0427@gmail.com>
2026-06-15 01:25:32 +0700

    feat(keybinds): add Senate Zoom daily/weekly links to leader Links layer

--- modules/hammerspoon/config/main.lua
+++ modules/hammerspoon/config/main.lua
@@ -150,6 +150,8 @@ spoon.LeaderFlow:setup({
             { "s", "senate-exchange repos", url("https://github.com/orgs/senate-exchange/repositories") },
             { "y", "youtube.com", url("https://youtube.com") },
             { "m", "google.com/maps", url("https://www.google.com/maps") },
+            { "d", "Senate daily", url("https://us06web.zoom.us/j/84129720700?pwd=Czei5trGxXmY6TfREpHfjVGiNuLGBH.1&jst=2") },
+            { "w", "Senate weekly", url("https://us06web.zoom.us/j/81435225054?pwd=6uR77sp5Oq2hvFtei463Vghub9jpY5.1&jst=2") },
             { "l", "[localhost]", {
                     { "c", "CRM", url("http://localhost:4003") },
                     { "t", "TG-Mini", url("http://localhost:8004") },

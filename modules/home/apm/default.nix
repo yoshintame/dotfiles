d99577ba67906dcccfadbdd7e22d91c1454dc27b
Mikhail Ivanov <m.ivanov0427@gmail.com>
2026-09-06 17:46:47 +0700

    chore(apm): persist global skill deployment lock

--- modules/home/apm/default.nix
+++ modules/home/apm/default.nix
@@ -1,6 +1,7 @@
 { config, myLib, ... }:
 myLib.mkModule config "apm" {
   nixLink.links = {
+    "~/.apm/apm.lock.yaml" = "modules/home/apm/config/apm.lock.yaml";
     "~/.apm/apm.yml" = "modules/home/apm/config/apm.yml";
   };
 }

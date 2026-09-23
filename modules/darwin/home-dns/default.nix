865c09a0bcc2710a3735acd688111110301cf6f9
Mikhail Ivanov <m.ivanov0427@gmail.com>
2026-09-24 02:07:39 +0700

    fix(darwin): identify home LAN by gateway MAC in home-dns
    - require the DHCP gateway MAC to match the home router in addition to AdGuard being offered as DNS
    - ping the gateway first so the ARP entry exists right after a network change

--- modules/darwin/home-dns/default.nix
+++ modules/darwin/home-dns/default.nix
@@ -7,13 +7,22 @@
 let
   service = "Wi-Fi";
   adguard = "192.168.1.48";
+  homeRouterMac = "64:20:e3:6e:af:12";
 
   switchDns = pkgs.writeShellScript "home-dns" ''
     set -u
     device=$(/usr/sbin/networksetup -listallhardwareports | /usr/bin/awk '$0 == "Hardware Port: ${service}" { getline; print $2 }')
-    offered=$(/usr/sbin/ipconfig getpacket "$device" 2>/dev/null | /usr/bin/grep '^domain_name_server' | /usr/bin/tr -d '{},')
+    packet=$(/usr/sbin/ipconfig getpacket "$device" 2>/dev/null)
+    offered=$(printf '%s\n' "$packet" | /usr/bin/grep '^domain_name_server' | /usr/bin/tr -d '{},')
+    gateway=$(printf '%s\n' "$packet" | /usr/bin/awk -F'[{},]' '/^router/ { print $2 }')
 
-    if printf '%s\n' $offered | /usr/bin/grep -qx '${adguard}'; then
+    gatewayMac=""
+    if [ -n "$gateway" ]; then
+      /sbin/ping -c 1 -t 1 "$gateway" >/dev/null 2>&1
+      gatewayMac=$(/usr/sbin/arp -n "$gateway" | /usr/bin/awk '{ print $4 }')
+    fi
+
+    if [ "$gatewayMac" = '${homeRouterMac}' ] && printf '%s\n' $offered | /usr/bin/grep -qx '${adguard}'; then
       want='${adguard}'
     else
       want='Empty'
